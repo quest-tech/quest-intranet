78e577bef53964e5b4986592915e232ffdfd8272
sliang100 <60456961+sliang100@users.noreply.github.com>
2020-11-28 16:51:33 -0500

    testing calendar component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Calendar from './components/Calendar';
 
 
 function App() {
   return (
     <>
       <Router>
-        <Navbar />
+        <Navbar/>
+        <Calendar/>
         <Switch>
           <Route path='/' exact component={Home} />
         </Switch>
