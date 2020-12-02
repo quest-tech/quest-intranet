de3ec099a2787e4463f7844c646335b5ae03afa2
Sathvik Ravi <sathvikrav@gmail.com>
2020-12-02 17:29:25 -0500

    changed the css of the announcements and calendar button to make it more seamless

--- src/components/Announcements.js
+++ src/components/Announcements.js
@@ -27,7 +27,7 @@ class Announcements extends React.Component {
       let jsx;
 
       if (this.state.isAnnouncement === true) {
-        jsx = <Nav.Item><img src="https://www.thesun.co.uk/wp-content/uploads/2020/11/5299d6ba-ab23-4459-8b82-f21476c053ec.jpg" alt="Missing Image" width="1000" height="600" /></Nav.Item>
+        jsx = <Nav.Item><img src="https://www.wiki-calendar.com/wp-content/uploads/2019/12/November-2020-Calendar.jpg" alt="Missing Image" width="1000" height="600" /></Nav.Item>
       } else {
         jsx = <Nav.Item><img src="https://www.wiki-calendar.com/wp-content/uploads/2019/12/November-2020-Calendar.jpg" alt="Missing Image" width="1000" height="600" /></Nav.Item>
       }
@@ -36,38 +36,21 @@ class Announcements extends React.Component {
         <div>
           <Nav className="justify-content-center" variant="pills" onSelect={this.switchContent}>
             <Nav.Item>
-              <Nav.Link eventKey="announcements">Announcements</Nav.Link>
+              <Nav.Link className={this.state.isAnnouncement ? 'active' : 'myClass'} eventKey="announcements">Announcements</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link eventKey="calendars">Calendar</Nav.Link>
+              <Nav.Link className={this.state.isAnnouncement ? 'myClass' : 'active'} eventKey="calendars">Calendar</Nav.Link>
             </Nav.Item>
           </Nav>
           <Nav className="justify-content-center">
             { jsx }
           </Nav>
         </div>
-        
-        /* <div class='container my-container' >
-            <ul class="nav nav-pills mb-3 " id="pills-tab" role="tablist">
-                <li class="nav-item">
-                    <a class="nav-link active" id="pills-announcements-tab" data-toggle="pill" href="#pills-announcements" role="tab" aria-controls="pills-announcements" aria-selected="true">Announcements</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" id="pills-calendar-tab" data-toggle="pill" href="#pills-calendar" role="tab" aria-controls="pills-calendar" aria-selected="false">Calendar</a>
-                </li>
-            </ul>
-            <div class="tab-content" id="pills-tabContent">
-                <div class="tab-pane fade show active" id="pills-announcements" role="tabpanel" aria-labelledby="pills-announcements-tab">
-                    <img src="https://www.wiki-calendar.com/wp-content/uploads/2019/12/November-2020-Calendar.jpg" alt="Missing Image" width="1000" height="600" />
-                </div>
-                <div class="tab-pane fade" id="pills-calendar" role="tabpanel" aria-labelledby="pills-calendar-tab">
-                    <img src="https://www.wiki-calendar.com/wp-content/uploads/2019/12/November-2020-Calendar.jpg" alt="Missing Image" width="1000" height="600" />
-                </div>
-            </div>
-        </div> */
     )
     
     }
   }
 
   export default Announcements;
+
+  //{this.state.isAnnouncement ? "active" : "myClass"}
